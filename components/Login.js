223608a6bb3a5423a01af771124acfc11f3034ae
Abdullah000001 <geniusswork00@gmail.com>
2021-12-17 23:53:25 +0500

    changings

--- components/Login.js
+++ components/Login.js
@@ -7,7 +7,7 @@ function Login() {
             <div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
                 <Image 
                     className="object-cover rounded-full"
-                    src="https://links.papareact.com/3pi"
+                    src="https://cdn01.vulcanpost.com/wp-uploads/2021/10/Andy-Warhol-Painting-to-be-Centerpiece-at-First-Crypto-Art-Auction.jpeg"
                     width={200} height={200}
                 />
                 <button
@@ -19,7 +19,7 @@ function Login() {
             </div>
             <div className="w-full h-screen">
                 <Image
-                    src="https://links.papareact.com/55n"
+                    src="https://www.universitymagazine.ca/wp-content/uploads/2021/09/How-To-Create-NFT-Art-Online.jpg"
                     layout='fill'
                     objectFit='cover'
                 />
--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     images:{
-        domains:["avatars.dicebear.com","links.papareact.com"]
+        domains:["avatars.dicebear.com","links.papareact.com","cdn01.vulcanpost.com","www.universitymagazine.ca"]
     }
 }
